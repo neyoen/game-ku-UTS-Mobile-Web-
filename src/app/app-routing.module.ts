68cd8b9ee623931a21ae8d6800cc4a31d32ca75b
neyoen <arioncamara34@gmail.com>
2021-10-29 15:00:00 +0700

    Pengumpulan UTS Mobile Web

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProfileComponent } from './profile/profile.component';
+import { HomeComponent } from './home/home.component';
+import { LeaderboardComponent } from './leaderboard/leaderboard.component';
+import { TournamentComponent } from './tournament/tournament.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'profile', component:ProfileComponent},
+  {path:'home', component:HomeComponent},
+  {path:'tournament', component:TournamentComponent},
+  {path:'leaderboard', component:LeaderboardComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
